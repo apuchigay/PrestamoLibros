e9ac019cfda9d3250f05723c5c0dba03f5617d68
Andres Puchigay <apuchigay@gmail.com>
2024-10-19 22:17:28 -0500

    cambio de diseño en pagina de miembros

--- app/src/main/java/com/example/prestamolibros/Screen/MemberScreen.kt
+++ app/src/main/java/com/example/prestamolibros/Screen/MemberScreen.kt
@@ -1,17 +1,23 @@
 package com.example.prestamolibros.Screen
 
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
@@ -161,111 +167,161 @@ class MemberViewModelFactory(private val repository: MemberRepository) : ViewMod
 fun MemberForm(viewModel: MemberViewModel, navController: NavController) {
     val coroutineScope = rememberCoroutineScope()
 
+    // Estado para controlar la visibilidad de la lista de miembros
+    var isListVisible by remember { mutableStateOf(false) }
+
+    // Fondo degradado
+    val gradient = Brush.horizontalGradient(
+        colors = listOf(Color(0xFF0b76d6), Color(0xFF0eb3aa))
+    )
+
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .background(gradient)
             .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center // Centrar todo verticalmente
     ) {
-        TextField(
-            value = viewModel.nombre,
-            onValueChange = { viewModel.nombre = it },
-            label = { Text("Nombre del Miembro") },
-            isError = viewModel.errorNombre.isNotEmpty(),
-            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next)
+        // Título
+        Text(
+            text = "Registrar Miembro Nuevo",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier.padding(bottom = 32.dp)
         )
-        if (viewModel.errorNombre.isNotEmpty()) {
-            Text(text = viewModel.errorNombre, color = MaterialTheme.colorScheme.error)
-        }
 
-        Spacer(modifier = Modifier.height(16.dp))
-
-        TextField(
-            value = viewModel.apellido,
-            onValueChange = { viewModel.apellido = it },
-            label = { Text("Apellido del Miembro") },
-            isError = viewModel.errorApellido.isNotEmpty(),
-            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next)
-        )
-        if (viewModel.errorApellido.isNotEmpty()) {
-            Text(text = viewModel.errorApellido, color = MaterialTheme.colorScheme.error)
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        TextField(
-            value = viewModel.fechaInscripcion,
-            onValueChange = { viewModel.fechaInscripcion = it },
-            label = { Text("Fecha de Inscripción") },
-            isError = viewModel.errorFechaInscripcion.isNotEmpty(),
-            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done)
-        )
-        if (viewModel.errorFechaInscripcion.isNotEmpty()) {
-            Text(text = viewModel.errorFechaInscripcion, color = MaterialTheme.colorScheme.error)
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        // Botón para registrar o actualizar el miembro
-        Button(onClick = {
-            coroutineScope.launch {
-                if (viewModel.isUpdating) {
-                    viewModel.updateMember() // Actualizar miembro
-                } else {
-                    viewModel.insertMember() // Registrar miembro
-                }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White, shape = RoundedCornerShape(8.dp))
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Campos de texto
+            TextField(
+                value = viewModel.nombre,
+                onValueChange = { viewModel.nombre = it },
+                label = { Text("Nombre del Miembro") },
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.Transparent
+                ),
+                isError = viewModel.errorNombre.isNotEmpty(),
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next)
+            )
+            if (viewModel.errorNombre.isNotEmpty()) {
+                Text(text = viewModel.errorNombre, color = MaterialTheme.colorScheme.error)
             }
-        }) {
-            Text(if (viewModel.isUpdating) "Actualizar Miembro" else "Registrar Miembro")
-        }
 
-        Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(16.dp))
 
-        // Botón para listar miembros
-        Button(onClick = {
-            viewModel.getMembers() // Llama a la función para obtener miembros
-        }) {
-            Text("Listar Miembros")
-        }
+            TextField(
+                value = viewModel.apellido,
+                onValueChange = { viewModel.apellido = it },
+                label = { Text("Apellido del Miembro") },
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.Transparent
+                ),
+                isError = viewModel.errorApellido.isNotEmpty(),
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next)
+            )
+            if (viewModel.errorApellido.isNotEmpty()) {
+                Text(text = viewModel.errorApellido, color = MaterialTheme.colorScheme.error)
+            }
 
-        Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(16.dp))
 
-        // Botón para volver al menú principal
-        Button(onClick = { navController.navigate("main_screen") }) {
-            Text(text = "Volver al Menú Principal")
-        }
+            TextField(
+                value = viewModel.fechaInscripcion,
+                onValueChange = { viewModel.fechaInscripcion = it },
+                label = { Text("Fecha de Inscripción") },
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.Transparent
+                ),
+                isError = viewModel.errorFechaInscripcion.isNotEmpty(),
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done)
+            )
+            if (viewModel.errorFechaInscripcion.isNotEmpty()) {
+                Text(text = viewModel.errorFechaInscripcion, color = MaterialTheme.colorScheme.error)
+            }
 
-        Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(16.dp))
 
-        // Mostrar la lista de miembros usando LazyColumn
-        LazyColumn {
-            items(viewModel.membersList) { member ->
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp)
-                ) {
-                    Column(modifier = Modifier.padding(16.dp)) {
-                        Text(text = "Nombre: ${member.nombre} ${member.apellido}")
-                        Text(text = "Fecha de Inscripción: ${member.fechaInscripcion}")
+            // Botón para registrar o actualizar el miembro
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        if (viewModel.isUpdating) {
+                            viewModel.updateMember()
+                        } else {
+                            viewModel.insertMember()
+                        }
+                    }
+                },
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF0b76d6))
+            ) {
+                Text(if (viewModel.isUpdating) "Actualizar Miembro" else "Registrar Miembro")
+            }
 
-                        Spacer(modifier = Modifier.height(8.dp))
+            Spacer(modifier = Modifier.height(16.dp))
 
-                        Row {
-                            // Botón para actualizar miembro
-                            Button(onClick = {
-                                viewModel.updateMemberDetails(member) // Cargar datos del miembro para actualizar
-                            }) {
-                                Text("Actualizar")
-                            }
+            // Botón para listar miembros
+            Button(
+                onClick = {
+                    viewModel.getMembers()
+                    isListVisible = true // Mostrar la lista cuando se haga clic
+                },
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF0b76d6))
+            ) {
+                Text("Listar Miembros")
+            }
 
-                            Spacer(modifier = Modifier.width(8.dp))
+            Spacer(modifier = Modifier.height(16.dp))
 
-                            // Botón para eliminar miembro
-                            Button(onClick = {
-                                viewModel.deleteMember(member) // Eliminar el miembro
-                            }) {
-                                Text("Eliminar")
+            // Botón para volver al menú principal
+            Button(
+                onClick = { navController.navigate("main_screen") },
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF0b76d6))
+            ) {
+                Text(text = "Volver al Menú Principal")
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Mostrar la lista de miembros solo si isListVisible es true
+            if (isListVisible) {
+                LazyColumn {
+                    items(viewModel.membersList) { member ->
+                        Card(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp)
+                        ) {
+                            Column(modifier = Modifier.padding(16.dp)) {
+                                Text(text = "Nombre: ${member.nombre} ${member.apellido}")
+                                Text(text = "Fecha de Inscripción: ${member.fechaInscripcion}")
+
+                                Spacer(modifier = Modifier.height(8.dp))
+
+                                Row {
+                                    Button(onClick = {
+                                        viewModel.updateMemberDetails(member)
+                                    }, colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF0b76d6))) {
+                                        Text("Actualizar")
+                                    }
+
+                                    Spacer(modifier = Modifier.width(8.dp))
+
+                                    Button(onClick = {
+                                        viewModel.deleteMember(member)
+                                    }, colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF0b76d6))) {
+                                        Text("Eliminar")
+                                    }
+                                }
                             }
                         }
                     }
@@ -279,6 +335,9 @@ fun MemberForm(viewModel: MemberViewModel, navController: NavController) {
         Toast.makeText(LocalContext.current, viewModel.successMessage, Toast.LENGTH_SHORT).show()
     }
 }
+
+
+
 
 @Composable
 fun MemberScreen(navController: NavController) {
